c83c46fa9aa0c709845f18dea4593bb9d7e507b2
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2023-11-16 20:10:05 -0500

    Refactor Container.java

--- src/main/java/com/lukeoldenburg/g2d2/client/gfx/ui/Container.java
+++ src/main/java/com/lukeoldenburg/g2d2/client/gfx/ui/Container.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class Container extends UIElement {
 	public List<UIElement> children = new ArrayList<>();
 	public boolean lockedWidth, lockedHeight;
+	private static final Color BACKGROUND_COLOR = new Color(0, 0, 0, 210);
+	private static final Color BORDER_COLOR = new Color(255, 255, 255);
 
 	public Container(int x, int y, int renderPriority) {
 		this.x = x;
@@ -27,19 +29,10 @@ public class Container extends UIElement {
 
 	@Override
 	public void draw(Graphics2D g2) {
-		children.sort((a, b) -> {
-			if (a.renderPriority < b.renderPriority) return 1;
-			if (a.renderPriority > b.renderPriority) return -1;
-			return 0;
-		});
-		getWidth(g2);
-		getHeight(g2);
-		Color c = new Color(0, 0, 0, 210);
-		g2.setColor(c);
+		g2.setColor(BACKGROUND_COLOR);
 		g2.fillRoundRect(x, y, width + 1, height + 1, 35, 35);
 
-		c = new Color(255, 255, 255);
-		g2.setColor(c);
+		g2.setColor(BORDER_COLOR);
 		g2.setStroke(new BasicStroke(5));
 		g2.drawRoundRect(x + 5, y + 5, width - 10, height - 10, 25, 25);
 
@@ -65,6 +58,15 @@ public class Container extends UIElement {
 	@Override
 	public void refresh(Graphics2D g2) {
 		children.forEach(uiElement -> uiElement.refresh(g2));
+
+		children.sort((a, b) -> {
+			if (a.renderPriority < b.renderPriority) return 1;
+			if (a.renderPriority > b.renderPriority) return -1;
+			return 0;
+		});
+
+		getWidth(g2);
+		getHeight(g2);
 	}
 
 	@Override
