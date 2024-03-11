a45fd87f3a0fcdaedaf8b24724d51a6ff52737f5
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-03-11 01:09:00 -0400

    Add checkbox

---
+++ src/main/java/com/lukeoldenburg/g2d2/client/gfx/ui/Checkbox.java
@@ -0,0 +1,35 @@
+package com.lukeoldenburg.g2d2.client.gfx.ui;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class Checkbox extends UIElement {
+	private boolean checked = false;
+
+	public Checkbox(String id, UIElement parentElement, int renderPriority, int x, int y) {
+		super(id, parentElement, renderPriority, x, y);
+		width = 20;
+		height = 20;
+	}
+
+	@Override
+	public void draw(Graphics2D g2) {
+		g2.setColor(Color.white);
+		g2.setStroke(new BasicStroke(3));
+		g2.drawRect(x, y, width, height);
+		if (checked) g2.fillRect(x, y, width, height);
+	}
+
+	@Override
+	public void onClick(MouseEvent e) {
+		checked = !checked;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+}
